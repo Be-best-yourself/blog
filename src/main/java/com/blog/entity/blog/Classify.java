c9156e050b48e766f11168276f76599fdb7b1370
15939427850@163.com <15939427850@163.com>
2018-06-06 00:17:52 +0800

    增加属性

--- src/main/java/com/blog/entity/blog/Classify.java
+++ src/main/java/com/blog/entity/blog/Classify.java
@@ -8,6 +8,10 @@ public class Classify implements Serializable {
 
     private String classifyName;
 
+    private Integer classifyParentId;
+
+    private String classifyDescription;
+
     private Integer classifyUserId;
 
     private Date classifyCreateTime;
@@ -30,6 +34,22 @@ public class Classify implements Serializable {
 
     public void setClassifyName(String classifyName) {
         this.classifyName = classifyName == null ? null : classifyName.trim();
+    }
+
+    public Integer getClassifyParentId() {
+        return classifyParentId;
+    }
+
+    public void setClassifyParentId(Integer classifyParentId) {
+        this.classifyParentId = classifyParentId;
+    }
+
+    public String getClassifyDescription() {
+        return classifyDescription;
+    }
+
+    public void setClassifyDescription(String classifyDescription) {
+        this.classifyDescription = classifyDescription == null ? null : classifyDescription.trim();
     }
 
     public Integer getClassifyUserId() {
@@ -70,6 +90,8 @@ public class Classify implements Serializable {
         Classify other = (Classify) that;
         return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
             && (this.getClassifyName() == null ? other.getClassifyName() == null : this.getClassifyName().equals(other.getClassifyName()))
+            && (this.getClassifyParentId() == null ? other.getClassifyParentId() == null : this.getClassifyParentId().equals(other.getClassifyParentId()))
+            && (this.getClassifyDescription() == null ? other.getClassifyDescription() == null : this.getClassifyDescription().equals(other.getClassifyDescription()))
             && (this.getClassifyUserId() == null ? other.getClassifyUserId() == null : this.getClassifyUserId().equals(other.getClassifyUserId()))
             && (this.getClassifyCreateTime() == null ? other.getClassifyCreateTime() == null : this.getClassifyCreateTime().equals(other.getClassifyCreateTime()))
             && (this.getClassifyModifyTime() == null ? other.getClassifyModifyTime() == null : this.getClassifyModifyTime().equals(other.getClassifyModifyTime()));
@@ -81,6 +103,8 @@ public class Classify implements Serializable {
         int result = 1;
         result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
         result = prime * result + ((getClassifyName() == null) ? 0 : getClassifyName().hashCode());
+        result = prime * result + ((getClassifyParentId() == null) ? 0 : getClassifyParentId().hashCode());
+        result = prime * result + ((getClassifyDescription() == null) ? 0 : getClassifyDescription().hashCode());
         result = prime * result + ((getClassifyUserId() == null) ? 0 : getClassifyUserId().hashCode());
         result = prime * result + ((getClassifyCreateTime() == null) ? 0 : getClassifyCreateTime().hashCode());
         result = prime * result + ((getClassifyModifyTime() == null) ? 0 : getClassifyModifyTime().hashCode());
@@ -95,6 +119,8 @@ public class Classify implements Serializable {
         sb.append("Hash = ").append(hashCode());
         sb.append(", id=").append(id);
         sb.append(", classifyName=").append(classifyName);
+        sb.append(", classifyParentId=").append(classifyParentId);
+        sb.append(", classifyDescription=").append(classifyDescription);
         sb.append(", classifyUserId=").append(classifyUserId);
         sb.append(", classifyCreateTime=").append(classifyCreateTime);
         sb.append(", classifyModifyTime=").append(classifyModifyTime);
