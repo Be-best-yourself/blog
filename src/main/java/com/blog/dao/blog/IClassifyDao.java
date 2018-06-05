0a9c96f802f679d38adfa2c789837e553009eca2
15939427850@163.com <15939427850@163.com>
2018-06-06 00:16:42 +0800

    修改blog包下的dao层

--- src/main/java/com/blog/dao/blog/IClassifyDao.java
+++ src/main/java/com/blog/dao/blog/IClassifyDao.java
@@ -3,5 +3,5 @@ package com.blog.dao.blog;
 import com.blog.dao.IBaseDao;
 import com.blog.entity.blog.Classify;
 
-public interface IClassifyDao extends IBaseDao<Classify> {
+public interface IClassifyDao extends IBaseDao<Classify>{
 }
