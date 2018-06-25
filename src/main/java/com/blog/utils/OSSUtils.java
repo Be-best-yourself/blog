36017b5a3d670e27d22a088454838a8d593bc624
15939427850@163.com <15939427850@163.com>
2018-06-25 18:35:43 +0800

    oss外链生成工具

---
+++ src/main/java/com/blog/utils/OSSUtils.java
@@ -0,0 +1,50 @@
+package com.blog.utils;
+
+import java.net.URL;
+import java.util.Date;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.GeneratePresignedUrlRequest;
+
+public class OSSUtils {
+	/**
+	 * 替换页面中oss外链失效时间
+	 * @param html
+	 * @param endpoint
+	 * @param accessKeyId
+	 * @param accessKeySecret
+	 * @param expiration
+	 * @param bucketName
+	 * @return
+	 */
+	public static String replaceHtml(String html, String endpoint, String accessKeyId, String accessKeySecret,
+			Date expiration, String bucketName){
+		Document document = Jsoup.parse(html);
+		Elements imgTags = document.getElementsByTag("img");
+		for (Element imgTag : imgTags) {
+			String srcString = imgTag.attr("src");
+			String ossUrl = endpoint.split("//")[0] + "//" + bucketName + "." + endpoint.split("//")[1];
+			if (srcString.contains(ossUrl)) {
+				srcString = srcString.replaceAll(ossUrl + "/", "");
+				String ossKey = srcString.substring(0, srcString.indexOf("?"));
+				String url = getUrl(ossKey, endpoint, accessKeyId, accessKeySecret, expiration, bucketName);
+				imgTag.attr("src", url);
+			}
+		}
+		return document.toString();
+	}
+
+	private static String getUrl(String key, String endpoint, String accessKeyId, String accessKeySecret,
+			Date expiration, String bucketName) {
+		OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+		GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucketName, key);
+		generatePresignedUrlRequest.setExpiration(expiration);
+		URL url = ossClient.generatePresignedUrl(generatePresignedUrlRequest);
+		return url.toString();
+	}
+}
